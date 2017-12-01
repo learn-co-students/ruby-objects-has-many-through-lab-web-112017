35699019eb0a7339c6c32e4f6df5a8d1a7630aee
JMoloughney <github email address>
2017-11-30 23:38:47 -0500

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,41 +1,20 @@
-# class Doctor
-# 	attr_accessor :name, :appointments
-
-# 	def initialize(name)
-# 		@name = name
-# 		@appointments = []
-# 	end
-
-# 	def add_appointment(appointment)
-# 		@appointments << appointment
-# 		appointment.doctor = self
-# 	end
-
-# 	def patients
-# 		self.appointments.map do |app|
-# 			app.patient
-# 		end
-# 	end
-
-# end
-
 class Doctor
-  attr_accessor :name, :appointments
+	attr_accessor :name, :appointments
 
-  def initialize(name)
-    @name = name
-    @appointments = []
-  end
+	def initialize(name)
+		@name = name
+		@appointments = []
+	end
 
-  def add_appointment(appointment)
-    @appointments << appointment 
-    appointment.doctor = self
-  end
+	def add_appointment(appointment)
+		@appointments << appointment
+	end
 
-  def patients
-    self.appointments.map do |appointment| 
-    	appointment.patient
-    end
-  end
+	def patients
+		self.appointments.map do |app|
+			app.patient
+		end
+	end
 
-end 
+end
+
