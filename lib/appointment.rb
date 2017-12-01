c9ada8f631093f46c1909be4f00a9667563e4f8d
JMoloughney <github email address>
2017-11-30 23:24:15 -0500

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -8,13 +8,3 @@ class Appointment
 	end
 end
 
-# class Appointment
-#   attr_accessor :date, :doctor, :patient
-
-#   def initialize(date, doctor)
-#     @date = date
-#     @doctor = doctor
-#     doctor.add_appointment(self)
-#   end
-
-# end
